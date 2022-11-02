34077e7067df3a54f35492b58834e31f1297d3d1
rwacco <robinwacco11@hotmail.com>
2022-11-02 20:19:46 +0100

    iets

--- drivehooks.py
+++ drivehooks.py
@@ -20,27 +20,42 @@ class Hook():
     def new_entry(self, form_data):
         pass
 
+    def __str__(self) -> str:
+        pass
+
 
 def load_hooks(path="hooks/"):
     hooks = {}
     hook_files = [f"{path + f}" for f in os.listdir(path) if isfile(join(path, f)) and f[0] != '_']
     for f_path in hook_files:
-        hook = Hook(tomllib.load(f_path))
+        with open(f_path, 'rb') as f: 
+            hook = Hook(tomllib.load(f))
         hooks[hook.form_name] = hook
+    return hooks
 
 
 hooks = load_hooks()
+print(hooks)
 
 app = Quart(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=["POST"])
 async def root():
-    form_data = dict(request.form)
-    hook = hooks.get(form_data["Form name"])
+    form_data = dict(await request.form)
+    key = request.args.get("key") 
 
+    print(form_data)
+    print(form_data.keys())
+    print(key)
+
+    hook = hooks.get(form_data["form_name"])
+
+    # no existing hook registered
     if not hook:
         pass
+
+    return "kaas"
 
 
 app.run(host="0.0.0.0", port=8030)
